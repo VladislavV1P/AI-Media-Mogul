a2c0329f4894d3f4e7f693b9b40889afdc52a132
Vladislav Patrakov <gostreid1@gmail.com>
2023-04-28 11:23:39 +0300

    begin

---
+++ Ai Media Mogul/Ai Media Mogul/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ai Media Mogul
+//
+//  Created by Владислав Патраков on 28.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
